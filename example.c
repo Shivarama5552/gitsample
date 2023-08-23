be0ba3b6701b346a4a6ec3e6654bf263509b6916
shivarama <a.shivamuni@gmail.com>
2023-08-24 00:38:48 +0530

    commiting on example files and txt files

---
+++ example.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the n value:\n");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        printf("enter the n value:\n");
+        scanf("%d",&n);
+        while(n>0)
+        break;
+    }
+    return 0;
+}
